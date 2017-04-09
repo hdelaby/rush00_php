8ccee15e8fae1fa95b28e9abaede77f4b0069385
cseccia <cseccia@student.42.fr>
2017-04-09 19:21:10 +0200

    sql corec

--- admin_show_or.php
+++ admin_show_or.php
@@ -20,15 +20,22 @@ $order = get_order($link, $_GET['id']);
 if ($order)
 {
   $basket = unserialize($order['content']);
-	foreach($basket as $val)
-	{
-    $product = get_product($link, $val['id_product']);
+  if (is_array($basket))
+  {
+    foreach($basket as $val)
+	   {
+       $product = get_product($link, $val['id_product']);
         echo ("<div class='product'><h2>".$product['label']." (".((int)$product['price'] / 100)."€)</h2>");
 		    echo ("<p>Description: ".$product['description']."</p>");
 		    echo ("<p>Quantite <b>".$val['quantity']."</b></p>");
 		    echo ("<form action='upd_basket.php' method='post'></form></div>");
-	}
-  echo ("<h2 style='text-align: right;'>Total : ".($order['total'] / 100)."</h2>");
+	     }
+       echo ("<h2 style='text-align: right;'>Total : ".($order['total'] / 100)."</h2>");
+  }
+  else
+  {
+    echo ("<h2>Pas de produits dans la commande</h2>");
+  }
 }
 ?>
 </body>
