70c2f2db5c87fe6f4482e5cb6bc073b9aac55b34
Hugo Delaby <delaby.h@gmail.com>
2017-04-09 16:49:10 +0200

    total_basket function ok

--- basket.php
+++ basket.php
@@ -16,12 +16,10 @@ if ($_GET['error'] == 1)
 	echo "<h2 class='error-empty'>Derniere action non effectuee</h2>";
 if ($_SESSION['basket'])
 {
-	$total_price = 0;
 	$link = mysqli_connect('localhost', 'root', 'root', 'RUSH');
 	foreach($_SESSION['basket'] as $val)
 	{
 		$product = get_product($link, $val['id_product']);
-		$total_price += ((int)$product['price'] * (int)$val['quantity']);
         echo ("<div class='product'><h2>".$product['label']." (".((int)$product['price'] / 100)."€)</h2>");
 		echo ("<p>Description: ".$product['description']."</p>");
 		echo ("<p>Quantite <b>".$val['quantity']."</b></p>");
@@ -30,7 +28,8 @@ if ($_SESSION['basket'])
 			<button class='lessbutton' name='less' value='".$product['id_product']."'>-</button>
 			</form></div>");
 	}
-	echo "<h2 style='text-align: right;'>Total: ".($total_price / 100)."€</h2>";
+	include("total_basket.php");
+	echo "<h2 style='text-align: right;'>Total: ".(total_basket($_SESSION['basket']) / 100)."€</h2>";
 }
 if ($_SESSION['logged_in_user'])
 {
--- total_basket.php
+++ total_basket.php
@@ -2,9 +2,16 @@
 
 function	total_basket($basket)
 {
+	$link = mysqli_connect('localhost', 'root', 'root', 'RUSH');
+	$total = 0;
 	if (!$basket)
 		return 0;
-	foreach($basket as $key => $val
+	foreach($basket as $val)
+	{
+		$product = get_product($link, $val['id_product']);
+		$total += ($product['price'] * $val['quantity']);
+	}
+	return ($total);
 }
 
 ?>
